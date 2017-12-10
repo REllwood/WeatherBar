7c34506251fec5f1743f426332ac4a9e80011e56
rhysellwood <s3491191@student.rmit.edu.au>
2017-12-10 22:01:57 +1100

    Initial Commit

---
+++ WeatherBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeatherBar
+//
+//  Created by Rhys Ellwood on 10/12/17.
+//  Copyright © 2017 Rhys Ellwood. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
